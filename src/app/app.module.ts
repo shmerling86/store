327ee374b6ab227a1dc9ed9284b597dc900ae8a0
shmerling86 <shmerling86@gmail.com>
2020-06-15 13:53:03 +0300

    genrate componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './componenets/auth/auth.component';
+import { ItemComponent } from './componenets/item/item.component';
+import { CartComponent } from './componenets/cart/cart.component';
+import { CheckoutComponent } from './componenets/checkout/checkout.component';
+import { StoreComponent } from './componenets/store/store.component';
+import { ShoppingCartComponent } from './componenets/shopping-cart/shopping-cart.component';
+import { ReviewComponent } from './componenets/review/review.component';
+import { NavbarComponent } from './componenets/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ItemComponent,
+    CartComponent,
+    CheckoutComponent,
+    StoreComponent,
+    ShoppingCartComponent,
+    ReviewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
